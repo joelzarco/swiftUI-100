a5c085f96bf399fba47ba93fc82389401c87e1c1
joelzarco <joelzarco@me.com>
2022-11-04 16:04:20 -0600

    Day3: linear,radial and angular gradients

--- guessTheFlag/guessTheFlag/ContentView.swift
+++ guessTheFlag/guessTheFlag/ContentView.swift
@@ -12,8 +12,16 @@ struct ContentView: View {
         ZStack{
             // Color it's a view itself
 //            Color.black.frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
-            Color.primary
+//            Color.primary
             // adpative colors like Color.primary, depends on systems setting's dark mode
+            // gradients
+//            LinearGradient(gradient: Gradient(colors: [.white, .cyan]), startPoint: .top, endPoint: .bottom)
+            //Gradient with stops
+//            LinearGradient(stops: [Gradient.Stop(color: .pink, location: 0.45), Gradient.Stop(color: .black, location: 0.55)], startPoint: .top, endPoint: .bottom) // looks really cool!!!
+            // Radial Gradient
+            //RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+            // ANgular gradient
+            AngularGradient(gradient: Gradient(colors: [.red, .blue, .pink]), center: .center)
             VStack {
                 HStack{
                     Image(systemName: "globe")
