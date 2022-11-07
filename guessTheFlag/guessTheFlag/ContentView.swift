9417270485c05d54fb68eb26e5c05700cfab3a33
joelzarco <joelzarco@me.com>
2022-11-07 17:36:36 -0600

    Day 6-7: Basic game UI implemented, score left as challenge

--- guessTheFlag/guessTheFlag/ContentView.swift
+++ guessTheFlag/guessTheFlag/ContentView.swift
@@ -8,102 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showAlert = false
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var countries = ["estonia", "france", "germany", "ireland", "italy", "nigeria", "poland", "russia", "spain", "uk", "us"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var score : Int = 0
     
     var body: some View {
+//        LinearGradient(stops: [Gradient.Stop(color: .pink, location: 0.45), Gradient.Stop(color: .black, location: 0.55)], startPoint: .top, endPoint: .bottom)
         ZStack{
-            // Color it's a view itself
-//            Color.black.frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
-//            Color.primary
-            // adpative colors like Color.primary, depends on systems setting's dark mode
-            // gradients
-//            LinearGradient(gradient: Gradient(colors: [.white, .cyan]), startPoint: .top, endPoint: .bottom)
-            //Gradient with stops
-//            LinearGradient(stops: [Gradient.Stop(color: .pink, location: 0.45), Gradient.Stop(color: .black, location: 0.55)], startPoint: .top, endPoint: .bottom) // looks really cool!!!
-            // Radial Gradient
-            //RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
-            // ANgular gradient
-            //-------------------------------------------
-            
-//            AngularGradient(gradient: Gradient(colors: [.red, .blue, .pink]), center: .center)
-            VStack {
-                HStack{
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .foregroundColor(.blue)
-                    Image(systemName: "globe")
-                        .imageScale(.large)
+            Color.cyan.ignoresSafeArea()
+            VStack(spacing : 30){
+                VStack{
+                    Text("Tap the flag of:")
                         .foregroundColor(.white)
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .foregroundColor(.cyan)
-                }
-                HStack{
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .foregroundColor(.blue)
-                    Image(systemName: "globe")
-                        .imageScale(.large)
+                    Text(countries[correctAnswer])
                         .foregroundColor(.white)
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .foregroundColor(.cyan)
-                }
-                HStack{
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .foregroundColor(.blue)
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .foregroundColor(.white)
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .foregroundColor(.cyan)
-                }
-                // BUTTONS
-                Button("Button 1"){}
-                    .buttonStyle(.bordered)
-                Button("Button 2", role: .destructive){}// destructive make it red
-                    .buttonStyle(.bordered)
-                Button("Button 3"){}
-                    .buttonStyle(.borderedProminent)
-                    .tint(.mint) // .tint() rerers to button's background color
-                Button("Button 4", role: .destructive){}
-                    .buttonStyle(.borderedProminent)
-                // custom button
-                Button{
-                    print("Custom button was tapped")
-                }label: {
-                    Text("I dare you!")
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(.black)
-                }
-                // Button with system image
-                Button{
-                    print("Edit button was tapped")
-                }label: {
-                    Label("Edit", systemImage: "pencil")
-//                        .padding()
-//                        .foregroundColor(.white)
-//                        .background(.black)
-                }
-                // show alerts in swiftUI should be a funtion of @State
-                Button("Initiate sequence", role: .destructive){
-                    showAlert = true
-                }
-                .buttonStyle(.borderedProminent)
-                .alert("This is NOT a drill", isPresented: $showAlert){// $ goes back to false automatically
-                    Button("Launch", role: .destructive){}
-                    Button("Cancel", role: .cancel){}
-                } message: {
-                    Text("Seek shelter")
                 }
                 
+                ForEach(0..<3){ number in
+                    Button{
+                        // flag was tapped
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
             }
-        }.ignoresSafeArea()
-//        .padding()
+        }//Z
+        .alert(scoreTitle, isPresented: $showingScore){
+            Button("Continue", action : askQuestion)
+        }message: {
+            Text("Your score is:\(score)")
+        }
+    }//body
+    
+    func flagTapped(_ number : Int){
+        if(number == correctAnswer){
+            scoreTitle = "Correct"
+            score += 1
+        }else{
+            scoreTitle = "Wrong"
+        }
+        // to show animation
+        showingScore = true
     }
+    
+    func askQuestion(){
+        // will not compile unless properties are declared as @state
+        countries.shuffled()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
 }
 
 //struct ContentView_Previews: PreviewProvider {
---
+++ guessTheFlag/guessTheFlag/Overview.swift
@@ -0,0 +1,108 @@
+//
+//  Overview.swift
+//  guessTheFlag
+//
+//  Created by Johel Zarco on 07/11/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct Overview: View {
+    @State private var showAlert = false
+    
+    var body: some View {
+        ZStack{
+            // Color it's a view itself
+//            Color.black.frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+//            Color.primary
+            // adpative colors like Color.primary, depends on systems setting's dark mode
+            // gradients
+//            LinearGradient(gradient: Gradient(colors: [.white, .cyan]), startPoint: .top, endPoint: .bottom)
+            //Gradient with stops
+//            LinearGradient(stops: [Gradient.Stop(color: .pink, location: 0.45), Gradient.Stop(color: .black, location: 0.55)], startPoint: .top, endPoint: .bottom) // looks really cool!!!
+            // Radial Gradient
+            //RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+            // ANgular gradient
+            //-------------------------------------------
+            
+//            AngularGradient(gradient: Gradient(colors: [.red, .blue, .pink]), center: .center)
+            VStack {
+                HStack{
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.blue)
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.white)
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.cyan)
+                }
+                HStack{
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.blue)
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.white)
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.cyan)
+                }
+                HStack{
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.blue)
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.white)
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.cyan)
+                }
+                // BUTTONS
+                Button("Button 1"){}
+                    .buttonStyle(.bordered)
+                Button("Button 2", role: .destructive){}// destructive make it red
+                    .buttonStyle(.bordered)
+                Button("Button 3"){}
+                    .buttonStyle(.borderedProminent)
+                    .tint(.mint) // .tint() rerers to button's background color
+                Button("Button 4", role: .destructive){}
+                    .buttonStyle(.borderedProminent)
+                // custom button
+                Button{
+                    print("Custom button was tapped")
+                }label: {
+                    Text("I dare you!")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.black)
+                }
+                // Button with system image
+                Button{
+                    print("Edit button was tapped")
+                }label: {
+                    Label("Edit", systemImage: "pencil")
+//                        .padding()
+//                        .foregroundColor(.white)
+//                        .background(.black)
+                }
+                // show alerts in swiftUI should be a funtion of @State
+                Button("Initiate sequence", role: .destructive){
+                    showAlert = true
+                }
+                .buttonStyle(.borderedProminent)
+                .alert("This is NOT a drill", isPresented: $showAlert){// $ goes back to false automatically
+                    Button("Launch", role: .destructive){}
+                    Button("Cancel", role: .cancel){}
+                } message: {
+                    Text("Seek shelter")
+                }
+                
+            }
+        }.ignoresSafeArea()
+//        .padding()
+    }
+}
